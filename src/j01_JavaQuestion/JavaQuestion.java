d38c9aba9937103bb4f4048c34a2142043390515
gezginss <omerfarukkiyak51@gmail.com>
2022-11-02 23:59:33 +0300

    Initial commit

---
+++ src/j01_JavaQuestion/JavaQuestion.java
@@ -0,0 +1,24 @@
+package j01_JavaQuestion;
+
+public class JavaQuestion {
+    public static void main(String[] args) {
+
+     //1-How to find the sum of two numbers?
+
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Numbers : " + num1 + " " + num2);
+        int total = num1 + num2;
+
+        System.out.println("total: " + total);
+
+
+
+
+
+
+
+
+    }
+}
